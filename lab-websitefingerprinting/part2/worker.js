c5b396fa7e5dd48dff9d69cbf1be9f571d34ec59
emsesc <emseschen@gmail.com>
2026-01-25 15:15:40 -0500

    Part 1 & Part 2

--- lab-websitefingerprinting/part2/worker.js
+++ lab-websitefingerprinting/part2/worker.js
@@ -1,6 +1,5 @@
-// Number of sweep counts
-// TODO (Exercise 2-1): Choose an appropriate value!
-let P = 1000;
+// P is the amount of time we spend counting in each interval (in ms)
+let P = 0.1; // The smallest time interval that we can measure with performance.now() in ms
 
 // Number of elements in your trace
 let K = 5 * 1000 / P; 
@@ -18,10 +17,34 @@ function record() {
   // Fill array with -1 so we can be sure memory is allocated
   T.fill(-1, 0, T.length);
 
-  // Save start timestamp
-  start = performance.now();
+  // Counts form a trace with length of K
+  let elapsed = 0;
+  let index = 0;
+  const LINE_SIZE = 16; // 128/sizeof(double)
+  const NUM_LINES = 1000; // At least 1000 cache lines need to be accessed to see resolution
+  const M = new Array(NUM_LINES * LINE_SIZE).fill(-1); // Fill with -1 to ensure allocation
 
-  // TODO (Exercise 2-1): Record data for 5 seconds and save values to T.
+  while (elapsed < 5000) { // Repeat sweep counting for 5 seconds
+    let counter = 0;
+    let beginTime = performance.now();
+
+    while (performance.now() - beginTime < P) {
+      counter++;
+
+      // Access cache lines sequentially, a "sweep" over the LLC
+      for (let j = 0; j < NUM_LINES; j++) {
+        let val = M[j * LINE_SIZE]; // Access one element per cache line
+      }
+    }
+
+    // After P milliseconds, record the counter value in T
+    if (index < K) {
+      T[index] = counter;
+    }
+
+    elapsed += P; // Increment elapsed time
+    index++;
+  }
 
   // Once done recording, send result to main thread
   postMessage(JSON.stringify(T));
